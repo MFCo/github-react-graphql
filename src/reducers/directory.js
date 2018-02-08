ba0d44872473d88bf92460d2b905308eda0e813f
mfc-boost <marianococirio@gmail.com>
2018-02-08 17:29:42 -0300

    minor changes

--- src/reducers/directory.js
+++ src/reducers/directory.js
@@ -2,14 +2,12 @@ import { handleActions } from 'redux-actions';
 
 const defaultState = {
   list: [],
-  fatherId: [],
   id: null
 };
 
 export default handleActions({
   UPDATE_DIRECTORY: (state, { payload: { newList, newId } }) => {
     return {
-      fatherId: state.fatherId,
       list: newList.sort((a, b) => a.type !== 'tree'),
       id: newId
     }
